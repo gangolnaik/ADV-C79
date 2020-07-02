f5ebb35742a50b91f52cfeb12f543699ababa5ca
Rupin <rupin@whitehatjr.com>
2020-07-02 17:59:12 +0530

    master

--- main.js
+++ main.js
@@ -20,17 +20,30 @@
         // console.log(menu_list_array);
 
         // document.getElementById("add_item").style.display = "";
-        document.getElementById("display_addedmenu").innerHTML = menu_list_array;
+        //document.getElementById("display_addedmenu").innerHTML = menu_list_array;
        // document.getElementById("sortbtn").style.display = "inline-block";
 
-        htmldata="<ol class='menulist'>"
+        // htmldata="<ol class='menulist'>"
+        // menu_list_array.sort();
+        // for(var i=0;i<menu_list_array.length;i++){
+        //     //document.getElementById('display_menu').innerHTML ='<li>' + menu_list_array[i] + '</li>'
+        //     htmldata=htmldata+'<li>' + menu_list_array[i] + '</li>'
+        // //    document.getElementById("display_menu").innerHTML = menu_list_array[i];
+        // }
+        //  htmldata=htmldata+"</ol>"
+        //   document.getElementById("display_addedmenu").innerHTML = htmldata;
+
+        htmldata="<div class='pizzacard'">
         menu_list_array.sort();
         for(var i=0;i<menu_list_array.length;i++){
             //document.getElementById('display_menu').innerHTML ='<li>' + menu_list_array[i] + '</li>'
+            <img>
+            <p>
+            
             htmldata=htmldata+'<li>' + menu_list_array[i] + '</li>'
         //    document.getElementById("display_menu").innerHTML = menu_list_array[i];
         }
-         htmldata=htmldata+"</ol>"
+         htmldata=htmldata+"</div>"
           document.getElementById("display_addedmenu").innerHTML = htmldata;
     }
 
